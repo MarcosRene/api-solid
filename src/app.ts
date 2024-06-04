e764455c317c5735625a5c20de1d6ee778940e9d
Marcos Rene <renemoura789@gmail.com>
2024-06-04 12:58:39 -0300

    feat: prisma setup

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
 import fastify from 'fastify'
 
+import { PrismaClient } from '@prisma/client'
+
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Marcos Renê',
+    email: 'marcosrenedev@gmail.com',
+  },
+})
