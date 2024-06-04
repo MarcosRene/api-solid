8d55f9909b372f8d882f648f249356fd4cbe47e2
Marcos Rene <renemoura789@gmail.com>
2024-06-04 14:06:18 -0300

    feat: add relationships

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,3 @@
 import fastify from 'fastify'
 
-import { PrismaClient } from '@prisma/client'
-
 export const app = fastify()
-
-const prisma = new PrismaClient()
-
-prisma.user.create({
-  data: {
-    name: 'Marcos Renê',
-    email: 'marcosrenedev@gmail.com',
-  },
-})
